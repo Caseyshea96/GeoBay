a6e398d1f29c78933d27858edc36a11adc50ccac
Caseyshea96 <144255051+Caseyshea96@users.noreply.github.com>
2025-03-31 10:25:07 -0400

    Update __init__.py

--- GeoBay/__init__.py
+++ GeoBay/__init__.py
@@ -5,4 +5,4 @@ __email__ = "shea96@gmail.com"
 __version__ = "0.0.6"
 
 """GeoBay package initialization."""
-from .GeoBay import CustomIpyleafletMap  # Adjust module name accordingly
+from .geobay import CustomIpyleafletMap  # Adjust module name accordingly
