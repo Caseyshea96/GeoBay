337d9fe4e210182ada73b7317b72447a097d7619
Casey Shea <shea96@gmail.com>
2025-03-31 10:36:42 -0400

    fdas

--- GeoBay/__init__.py
+++ GeoBay/__init__.py
@@ -1,8 +1,1 @@
-"""Top-level package for Geobay."""
-
-__author__ = """Casey Shea"""
-__email__ = "shea96@gmail.com"
-__version__ = "0.0.6"
-
-"""GeoBay package initialization."""
-from GeoBay import CustomIpyleafletMap  # Adjust module name accordingly
+from .geobay import CustomIpyleafletMap, FoliumMap  # Use relative import
